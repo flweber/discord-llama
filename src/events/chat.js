59a118616b78aa9cb2c9d2bfc1039050a01fbc81
Florian <f.weber@flweber.me>
2024-06-11 23:38:14 +0200

    Added option to use chromadb as vector store

--- src/events/chat.js
+++ src/events/chat.js
@@ -7,18 +7,24 @@ module.exports = async (message, rag) => {
     message.channel.sendTyping();
     const embedding = await rag.createEmbedding(message.content);
     const searchResults = await rag.search(embedding);
-    _.forEach(searchResults.results, result => console.log(`${result.name}: ${result.score}`));
-    if (searchResults.results.length <= 0) {
-      throw { message: "Nothing found" };
-    } else {
-      let answer = "";
+    let answer = "";
+    if (rag.backend !== "chromadb") {
+      _.forEach(searchResults.results, result => console.log(`${result.name}: ${result.score}`));
+      if (searchResults.results.length <= 0) {
+        throw { message: "Nothing found" };
+      }
       const result = _.sortBy(searchResults.results, ["score"])[searchResults.results.length - 1];
       console.log(`\n\nDocument: ${result.name}\nScore: ${result.score}\n\n`);
       if (result.score < 250) throw new Error("Sorry we don't have any data to your question");
       answer = await rag.answerQuestion(message.content, result.name);
-      console.log(answer);
-      message.author.send(answer);
+    } else {
+      if (searchResults.documents.length <= 0) {
+        throw { message: "Nothing found" };
+      }
+      console.log("Found documents: ", searchResults.documents);
+      answer = await rag.answerQuestion(message.content, searchResults.documents[0]);
     }
+    message.author.send(answer);
   } catch (err) {
     message.author.send(err.message ?? err);
   } finally {
--- src/rag.js
+++ src/rag.js
@@ -1,34 +1,43 @@
-const Ollama = require('ollama').Ollama;
-const MilvusClient = require('@zilliz/milvus2-sdk-node').MilvusClient;
-const DataType = require('@zilliz/milvus2-sdk-node').DataType;
+const Ollama = require("ollama").Ollama;
+const MilvusClient = require("@zilliz/milvus2-sdk-node").MilvusClient;
+const DataType = require("@zilliz/milvus2-sdk-node").DataType;
 const PDFParser = require("pdf2json");
 const path = require("node:path");
 const fs = require("node:fs");
+const { ChromaClient } = require("chromadb");
 
 class RAG {
   constructor() {
-    this.milvus = undefined;
     this.ollama = undefined;
-    this.jsonPath = path.join(__dirname, "documents.json");
+    this.backend = process.env.DB_BACKEND;
+
+    if (this.backend === "chromadb") {
+      this.chromaclient = new ChromaClient({ path: process.env.CHROMA_HOST });
+      this.collection = undefined;
+      this.milvus = undefined;
+      this.jsonPath = undefined;
+    } else {
+      this.milvus = new MilvusClient({ address: process.env.MILVUS_ADDRESS });
+      this.jsonPath = path.join(__dirname, "documents.json");
+      this.chromaclient = undefined;
+      this.collection = undefined;
+    }
   }
 
-
   async loadCollection() {
-    await this.milvus.loadCollectionSync({
-      collection_name: "rag_data",
-    });
+    if (this.backend !== "chromadb") {
+      await this.milvus.loadCollectionSync({
+        collection_name: "rag_data",
+      });
+    }
   }
 
   async init() {
-    const address = process.env.MILVUS_ADDRESS;
+    if (this.backend !== "chromadb") {
+      // connect to milvus
+      this.milvus.connect();
 
-    // connect to milvus
-    this.milvus = new MilvusClient({ address });
-
-    this.milvus.connect();
-
-    this.milvus.showCollections()
-      .then(res => {
+      this.milvus.showCollections().then((res) => {
         if (!res.collection_names.includes("rag_data")) {
           this.milvus.createCollection({
             collection_name: "rag_data",
@@ -51,34 +60,40 @@ class RAG {
                 name: "updated_timestamp",
                 description: "Timestamp when this document was last updated",
                 data_type: DataType.Int64,
-              }
-            ]
+              },
+            ],
           });
         }
       });
 
-    await this.milvus.createIndex({
-      collection_name: "rag_data",
-      field_name: 'embed',
-      index_name: 'test1',
-      index_type: 'FLAT',
-      params: { efConstruction: 10, M: 4 },
-      metric_type: 'L2',
-    });
+      await this.milvus.createIndex({
+        collection_name: "rag_data",
+        field_name: "embed",
+        index_name: "test1",
+        index_type: "FLAT",
+        params: { efConstruction: 10, M: 4 },
+        metric_type: "L2",
+      });
 
-    await this.loadCollection();
+      await this.loadCollection();
+    } else {
+      this.collection = await this.chromaclient.getOrCreateCollection({
+        name: "rag_data",
+      });
+    }
 
     this.ollama = new Ollama({ host: process.env.DC_OLLAMA_HOST });
   }
 
   async createEmbedding(prompt) {
     const embed = await this.ollama.embeddings({
-      model: 'mxbai-embed-large',
+      model: "mxbai-embed-large",
       keep_alive: 60000,
       prompt,
     });
     return embed.embedding;
   }
+
   async addDocument(name, url, isLocal = false) {
     if (isLocal) {
       console.warn("Local documents are not implemented.");
@@ -94,10 +109,10 @@ class RAG {
         const pdfParser = new PDFParser(this, 1);
         // Ein Promise erstellen, um die Verarbeitung abzuwarten
         const pdfPromise = new Promise((resolve, reject) => {
-          pdfParser.on("pdfParser_dataError", errData => reject(errData.parserError));
-          pdfParser.on("pdfParser_dataReady", pdfData => {
-            console.log(JSON.stringify(pdfData, undefined, 2));
-            console.log(pdfParser.getRawTextContent());
+          pdfParser.on("pdfParser_dataError", (errData) =>
+            reject(errData.parserError)
+          );
+          pdfParser.on("pdfParser_dataReady", (pdfData) => {
             resolve(pdfData);
           });
         });
@@ -107,33 +122,44 @@ class RAG {
         // Warten auf PDF zu JSON Konversion
         await pdfPromise;
 
-        const content = pdfParser.getRawTextContent();
-
-        console.log(content);
+        const content = pdfParser.getRawTextContent().replaceAll("\r\n", " ").replaceAll("\n", " ").replace(/-+(.*?)-+/gm, " ").trim();
 
         const embedding = await this.createEmbedding(content);
 
-        await this.milvus.upsert({
-          collection_name: "rag_data",
-          data: [{
-            name,
-            embed: embedding,
-            updated_timestamp: Date.now(),
-          }],
-        });
+        if (this.backend !== "chromadb") {
+          await this.milvus.upsert({
+            collection_name: "rag_data",
+            data: [
+              {
+                name,
+                embed: embedding,
+                updated_timestamp: Date.now(),
+              },
+            ],
+          });
 
-        await this.loadCollection();
+          await this.loadCollection();
 
-        let existingData = {};
-        if (fs.existsSync(this.jsonPath)) {
-          existingData = JSON.parse(fs.readFileSync(this.jsonPath, 'utf8'));
+          let existingData = {};
+          if (fs.existsSync(this.jsonPath)) {
+            existingData = JSON.parse(fs.readFileSync(this.jsonPath, "utf8"));
+          }
+
+          // Aktualisiere Daten
+          existingData[name] = content;
+          fs.writeFileSync(
+            this.jsonPath,
+            JSON.stringify(existingData, null, 2)
+          );
+        } else {
+          await this.collection.add({
+            ids: [name],
+            embeddings: [embedding],
+            documents: [content],
+          });
         }
 
-        // Aktualisiere Daten
-        existingData[name] = content;
-        fs.writeFileSync(this.jsonPath, JSON.stringify(existingData, null, 2));
-
-        console.log('Documents file has been updated.');
+        console.log("Documents file has been updated.");
       } catch (error) {
         throw new Error(`Error downloading or parsing PDF: ${error}`);
       }
@@ -141,72 +167,100 @@ class RAG {
   }
 
   async deleteDocument(name) {
-    const res = await this.milvus.delete({
-      collection_name: "rag_data",
-      filter: `name in ['${name}']`,
-    });
+    let res;
+    if (this.backend !== "chromadb") {
+      res = await this.milvus.delete({
+        collection_name: "rag_data",
+        filter: `name in ['${name}']`,
+      });
 
-    let existingData = {};
-    if (fs.existsSync(this.jsonPath)) {
-      existingData = JSON.parse(fs.readFileSync(this.jsonPath, 'utf8'));
-      delete existingData[name];
-      fs.writeFileSync(this.jsonPath, JSON.stringify(existingData, null, 2));
+      let existingData = {};
+      if (fs.existsSync(this.jsonPath)) {
+        existingData = JSON.parse(fs.readFileSync(this.jsonPath, "utf8"));
+        delete existingData[name];
+        fs.writeFileSync(this.jsonPath, JSON.stringify(existingData, null, 2));
+      }
+    } else {
+      res = await this.collection.delete({ ids: [name] });
     }
 
     return res;
   }
 
   async listDocuments() {
-    const res = await this.milvus.get({
-      collection_name: "rag_data",
-      limit: 100,
-    });
-
+    let res;
+    if (this.backend !== "chromadb") {
+      res = await this.milvus.get({
+        collection_name: "rag_data",
+        limit: 100,
+      });
+    } else {
+      res = await this.collection.get({ limit: 100, includes: ["ids"] });
+      res = res.ids;
+    }
+    
     return res;
   }
 
   async search(embedding) {
-    const res = await this.milvus.search({
-      collection_name: "rag_data",
-      data: embedding,
-      limit: 4,
-      params: { nprobe: 64 },
-      output_fields: ['name', 'embed'],
-    });
-
+    let res;
+    if (this.backend !== "chromadb") {
+      res = await this.milvus.search({
+        collection_name: "rag_data",
+        data: embedding,
+        limit: 4,
+        params: { nprobe: 64 },
+        output_fields: ["name", "embed"],
+      });  
+    } else {
+      res = await this.collection.query({
+        queryEmbeddings: [embedding],
+        nResults: 1,
+      });
+    }
+    
     return res;
   }
 
+  prepareContext(document) {
+    if (this.backend !== "chromadb") {
+      const readData = fs.readFileSync(this.jsonPath, "utf8");
+      const documents = JSON.parse(readData);
+      return documents[document];
+    } else {
+      return document[0];
+    }
+  }
+
   async answerQuestion(prompt, document, history = []) {
-    const readData = fs.readFileSync(this.jsonPath, 'utf8');
-    const documents = JSON.parse(readData);
-    console.log("Request ollama...")
+    const context = this.prepareContext(document);
+    console.log("Request ollama...");
     let answer = "";
     const chat = await this.ollama.chat({
-      model: 'llama3-chatqa',
+      model: "llama3-chatqa",
       keep_alive: 60000,
       messages: [
-        {
+        /*{
           role: "system",
           content: `You are a support bot which should help the user on specific requests.
           You're always polite and you're always answering in the language in which the user writes.
-          You are only allowed to use the following information to answer the request: ${documents[document]}.
-          Format the response using markdown and use not more than 2000 characters.`
-        },
+          If you cannot find relevant information in the context please let the user know that there are no information and stop generation.
+          You are only allowed to use the following information to answer the request: ${context}.
+          Format the response using markdown and use not more than 2000 characters.`,
+        },*/
         {
           role: "user",
-          content: prompt,
-        }
+          content: `Context: ${context}. Prompt: ${prompt}`,
+        },
       ],
       stream: true,
     });
     for await (const part of chat) {
       answer += part.message.content;
-      //process.stdout.write(part.message.content);
     }
     console.log("Request done.");
     return answer;
   }
-};
+}
 
 module.exports = RAG;
